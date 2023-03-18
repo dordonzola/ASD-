e5afd780476f71140cd810ef65402ec85c0b8589
dordonzola <127895783+dordonzola@users.noreply.github.com>
2023-03-18 22:07:47 +0100

    Update and rename zmiana repr. grafu.py to change of graph representation.py

--- change of graph representation.py
+++ change of graph representation.py
@@ -1,14 +1,14 @@
-'''z takiej:
+'''from this:
 [[0,1,0],\
  [1,0,1],\
  [0,1,0]]
 
-na taką:
+to that:
 [1],[0,2],[1]'''
 
 n1=len(G1)
     G=[[]for i in range(n1)]
-    for i in range(n1):#zmiana sposobu reprezentacji grafu
+    for i in range(n1):
         for j in range(n1):
             if G1[i][j]==1:
                 G[i].append(j)
